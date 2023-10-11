ef94773ed4cca45ff6169bf1d7f02dfb4f7bacff
prabhjyot009 <prabh156dps@gmail.com>
2023-10-11 17:08:04 +0530

    Relational operator in php

--- index.php
+++ index.php
@@ -1,46 +1,43 @@
 <?php
+
+# if conditional statement
+
+## Relational Operators
 /*
-   //Pre defined functions:
-   ## string functions
-   ## number functions
-   ## math functions
+    ==	Equal
+    ===	Identical
+    !=	Not Equal
+    <>	Not Equal
+    !==	Not Identical
+    >	Greater Than
+    <	Less Than
+    >=	Greater Than or Equal To
+    <=	Less Than or Equal To
 */
 
-//String fucntion
-
-$name="Singh brar";
-echo strlen($name)."<br>";
-echo str_replace("S","B",$name)."<br>";
-echo str_replace("s","b",$name)."<br>";//it is not case sensitive
-//case sensitive:
-echo str_ireplace("s","b",$name)."<br>";
-echo ucwords($name)."<br>";
-echo strtoupper($name)."<br>";
-echo strtolower($name)."<br>";
-echo strrev($name)."<br><br>";
-
-//Number functions
-$num=30;
-var_dump(is_int($num));
-echo "<br>";
-var_dump(is_float($num));
-echo "<br>";
-$num2=30.5;
-var_dump(is_numeric($num2));
-echo "<br>";
-var_dump(is_string($num));
-echo "<br>";
-var_dump(is_bool($num));
-
-//Math functions
+$age=20;
+if($age>=21 && $age<25){
+    echo "You can drink in the US";
+}
+else if($age>=18){
+    echo "You can drink in India";
+}
+else{
+    echo "You can't drink";
+}
 echo "<br><br>";
-echo pi()."<br>";
-echo min(0,150,30,20,-8,-200)."<br>";
-echo max(0,150,30,20,-8,-200)."<br>";
-echo abs(-6.7)."<br>";
-echo sqrt(64)."<br>";
-echo round(0.60)."<br>";
-echo round(0.49)."<br>";
-echo rand()."<br>";
-echo rand(10,100)."<br>";
+$hours=50; //8days*5hours
+$rate=10;
+$weekpay=0;
+
+if($hours==0){
+    $weekpay=0;
+}
+else if($hours <=40 ){
+    $weekpay=$hours*$rate;
+}
+else{
+    $weekpay=(40*$rate)+($hours-40)*($rate*1.5);
+}
+echo "Your weekly payment is :". $weekpay;
 ?>
