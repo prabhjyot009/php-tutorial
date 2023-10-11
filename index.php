f477bb4b459dd412f856eecb3285f3add82f6cb5
prabhjyot009 <prabh156dps@gmail.com>
2023-10-11 15:46:32 +0530

    operators in php

--- index.php
+++ index.php
@@ -1,29 +1,94 @@
 <?php
+//operators in php:
 /*
-  ## Variable:
-  A resuable container that holds the data
-
-  ## Data type:
-  string, integer, float, boolean
+   ## Airthmatic operators
+   + - * / ** %
 */
 
-$name="Singh"; //string
-$age=30; //integer
-$marks=81.23; //float
-$isMarried=true; //boolean
+$a=10;
+$b=2;
+$c=$a+$b;
+$d=$a-$b;
+$e=$a*$b;
+$f=$a/$b;
+$g=$a%$b; 
+$h=$a+$b;
+$i=$a**$b;
+// echo $a . "+" . $b . "=" . $c;
+echo "$c<br>";
+echo "$d<br>";
+echo "$e<br>";
+echo "$f<br>";
+echo "$g<br>";
+echo "$h<br>";
+echo "$i<br><br>";
 
-echo "My name is : ".$name;
-echo "<br>My age is : ".$age;
-echo "<br>My marks are : $marks %";
-echo "<br>Am I married : {$isMarried}";//no single quotation is allowed.
+/*
+   ##Increment/Decrement operators:
+   ++ --
+ */
+$x=10;
+ echo $x."<br>";
+ $x++;//post increment
+ echo $x."<br><br>";
 
-//to identify type of variable
-echo "<br><br>";
-var_dump($name);
-echo "<br>";
-var_dump($age);
-echo "<br>";
-var_dump($marks);
-echo "<br>";
-var_dump($isMarried);
+ $X=10;
+ echo $X."<br>";
+ $X--;//post decrement
+ echo $X."<br><br>";
+
+ $y=10;
+ echo $y."<br>";
+ ++$y;//pre increment
+ echo $y."<br><br>";
+
+ $Y=10;
+ echo $Y."<br>";
+ --$Y;//pre decrement
+ echo $Y."<br><br>";
+
+ $A=10;
+ $B=$A++;
+ $C=$A;
+ echo $A."<br>";
+ echo $B."<br>";
+ echo $C."<br><br>";
+
+ $D=10;
+ $E=++$D;
+ $F=$D;
+ echo $D."<br>";
+ echo $E."<br>";
+ echo $F."<br><br>";
+
+ $A=10;
+ $B=$A--;
+ $C=$A;
+ echo $A."<br>";
+ echo $B."<br>";
+ echo $C."<br><br>";
+
+ $D=10;
+ $E=--$D;
+ $F=$D;
+ echo $D."<br>";
+ echo $E."<br>";
+ echo $F."<br><br>";
+
+/*
+   ##Operators Precedence
+   ()
+   **
+   * / +
+   * + -
+*/
+
+$total= 4/2*3+(5+10)**2;
+// 4/2*3+15**2;
+// 4/2*3+225;
+// 2*3+225;
+// 6+225;
+// 231;
+echo $total;
+
 ?>
