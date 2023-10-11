d5fee19b02e3d553bfd5328a5009464efd768d54
prabhjyot009 <prabh156dps@gmail.com>
2023-10-11 19:18:21 +0530

    isset() and empty() in php

--- index.php
+++ index.php
@@ -1,44 +1,27 @@
 <?php
 /*
-  user defined function
-  function - is a block of statements that
-  can be used in a program
-  1: function call
-  2: function defination
+  ##isset()
+  Return TRUE, if a variable is declared and not null
+
+  ##empty()
+  Return True, if a variable is not declared, false, null, ""
 */
 
-//function define:
-// function myfun(){
-//     echo "Welcome from myFun<br>";
-// }
-
-// //function call:
-// myfun();
-
-// function sum(){
-//     echo 2+5;
-// }
-// sum();
-
-// $a=5;
-// $b=10;
-// sum($a,$b);
-// function sum($num1,$num2){
-//     echo $num1+$num2."<br>";
-// }
-// $a=10;
-// $b=13;
-// sum($a,$b);
-
-$a=5;
-$b=10;
-$total=sum($a,$b);
-echo $total."<br>";
-function sum($num1,$num2){
-    return $num1+$num2."<br>";
+$name="";
+// echo isset($name);
+if(isset($name)){
+    echo "Name is set";
 }
-$a=10;
-$b=13;
-$total=sum($a,$b);
-echo $total."<br>";
+else{
+    echo "Name is not set";
+}
+echo "<br><br>";
+$name1="";
+$name1=null;
+
+if(empty($name1)){
+    echo "Name is empty";
+}
+else
+    echo "Name is not empty";
 ?>
