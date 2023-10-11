2f92a0efa3d302e340e86bb5d20a14b383c18087
prabhjyot009 <prabh156dps@gmail.com>
2023-10-11 15:03:23 +0530

    Basics

--- index.php
+++ index.php
@@ -2,4 +2,23 @@
 
 echo "Hello";
 
+//Single line comment
+# single line comment
+/*double line comment*/
+
 ?>  
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP tutorial</title>
+</head>
+<body>
+    <h1>
+        <?php echo "Hello"?>
+    </h1>
+    <?php echo "sir"?>
+</body>
+</html>
