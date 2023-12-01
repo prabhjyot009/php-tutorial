754797cd06e2cceaf23daaa928d0e887cee3d685
prabhjyot009 <prabh156dps@gmail.com>
2023-12-02 01:34:16 +0530

    create table

---
+++ lms/saveStudentDetails.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Save Student Details</title>
+    </head>
+    <body>
+            <form>
+                    Student Name
+                    <input type="text" name="studentName" placeholder="Enter Student Name" required><br>
+                    Student ID
+                    <input type="text" name="studentID" placeholder="Enter Student ID" required><br>
+                    Contact Number
+                    <input type="phone" name="contactNumber" placeholder="Enter Contact Number" required><br><br>
+                    <button type="submit" name="submit">Save</button>
+            </form>
+    </body>
+</html>
